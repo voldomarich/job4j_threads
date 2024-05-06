f6678d0ce0330181e826ce60ddb01b5700436f45
voldomarich <vbekasov1987@gmail.com>
2024-05-06 23:03:14 +0300

    2. Создание и запуск нити [#1016 #493638]

---
+++ src/main/java/ru/job4j/ConcurrentOutput.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        second.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
