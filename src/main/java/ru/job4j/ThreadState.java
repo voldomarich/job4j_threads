3539682ae5f64b861084cf0a04fe856425edb08a
voldomarich <vbekasov1987@gmail.com>
2024-05-07 22:09:01 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/ThreadState.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+public class ThreadState {
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new Thread(
+                () -> { });
+        Thread second = new Thread(
+                () -> { });
+        System.out.println("Initial status:");
+        System.out.printf("Thread name: %s, thread status: %s %n", first.getName(), first.getState());
+        first.start();
+        System.out.printf("Thread name: %s, thread status: %s %n", second.getName(), second.getState());
+        second.start();
+
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.printf("Thread name: %s, thread status: %s %n", first.getName(), first.getState());
+            System.out.printf("Thread name: %s, thread status: %s %n", second.getName(), second.getState());
+        }
+        first.join();
+        second.join();
+
+        System.out.println("Both statuses before closing:");
+        System.out.printf("Thread name: %s, thread status: %s %n", first.getName(), first.getState());
+        System.out.printf("Thread name: %s, thread status: %s %n", second.getName(), second.getState());
+        System.out.println("Main over");
+    }
+}
