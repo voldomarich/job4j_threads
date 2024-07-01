279c308de3c32cffcf47f9ca834abf780ffaab11
voldomarich <vbekasov1987@gmail.com>
2024-07-01 19:23:26 +0300

    4. CompletableFuture [#361626]

---
+++ src/main/java/ru/job4j/pool/Sums.java
@@ -0,0 +1,38 @@
+package ru.job4j.pool;
+
+import java.util.Objects;
+
+public class Sums {
+    private final int rowSum;
+    private final int colSum;
+
+    public Sums(int rowSum, int colSum) {
+        this.rowSum = rowSum;
+        this.colSum = colSum;
+    }
+
+    public int getRowSum() {
+        return rowSum;
+    }
+
+    public int getColSum() {
+        return colSum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Sums sums = (Sums) o;
+        return rowSum == sums.rowSum && colSum == sums.colSum;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rowSum, colSum);
+    }
+}
